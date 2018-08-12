ae3e52dda22cd6f000d98b6d5fba1ad80dd73828
guilherme-pombo <cristovaopombo@gmail.com>
2018-08-12 23:01:51 +1000

    better generator code

--- generator.py
+++ generator.py
@@ -43,7 +43,7 @@ class SingleViewGen:
 
 
 class MultiViewGen:
-    def __init__(self, classes_filename, dataset, batch_size, target_size, num_classes, data_folder='view'):
+    def __init__(self, classes_filename, dataset, batch_size, target_size, num_classes, num_views=12, data_folder='view'):
         with open(os.path.join(data_folder, classes_filename), 'r') as cf:
             classes = cf.read().split('\n')[:-1]
         self.paths = []
@@ -60,6 +60,8 @@ class MultiViewGen:
         self.batch_size = batch_size
         self.target_size = target_size
         self.num_classes = num_classes
+        self.view_shape = (batch_size,) + target_size + (3,)
+        self.num_views = num_views
 
     def get_num_total_imgs(self):
         return len(self.paths)
@@ -67,20 +69,17 @@ class MultiViewGen:
     def generator(self):
         counter = 0
         while True:
-            X = []
             Y = []
-            for _ in range(self.batch_size):
+            views = [np.zeros(self.view_shape) for _ in range(self.num_views)]
+            for b_idx in range(self.batch_size):
                 c_idx, img_paths = self.paths[counter]
-                views = []
-                for img_path in img_paths:
+                for v_idx, img_path in enumerate(img_paths):
                     img_mat = image.img_to_array(image.load_img(img_path, target_size=self.target_size)) / 255.
-                    views.append(np.expand_dims(img_mat, axis=0))
-                X.append(views)
+                    views[v_idx][b_idx] = img_mat
                 y = np.zeros(self.num_classes)
                 y[c_idx] = 1
                 Y.append(y)
                 counter += 1
                 if counter >= len(self.paths):
                     counter = 0
-            # TODO: change behaviour to allow for batches of views
-            yield X[0], np.array(Y)
+            yield views, np.array(Y)
